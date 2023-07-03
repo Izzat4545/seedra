f2b522c2339cad51f521d58d5487f5dcfab9bcb3
Izzat4545 <izzatsultanov77@gmail.com>
2023-07-03 13:33:43 +0500

    sidenav has been fixed

---
+++ app.js
@@ -0,0 +1,15 @@
+const sidebar = document.querySelector('.sidebar')
+const triggerBtn = document.querySelector('.hamburger')
+const closeBtn = document.querySelector('.closeBtn')
+const body = document.querySelector('body')
+let trigger = false
+
+triggerBtn.addEventListener('click', triggerSidebar)
+closeBtn.addEventListener('click', close)
+
+function triggerSidebar(){
+    sidebar.style.right = '0%'
+}
+function close(){
+    sidebar.style.right = '-50%'
+}
