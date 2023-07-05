2998a172d6b329bcaf2cbebb4f74852f04065ceb
Izzat4545 <izzatsultanov77@gmail.com>
2023-07-05 21:22:51 +0500

    implemented product section

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const sidebar = document.querySelector('.sidebar')
 const triggerBtn = document.querySelector('.hamburger')
 const closeBtn = document.querySelector('.closeBtn')
 const body = document.querySelector('body')
+const like = document.querySelector('.card_favaurite_icon_passiv')
+const dislike = document.querySelector('.card_favaurite_icon_active')
 let trigger = false
 
-triggerBtn.addEventListener('click', triggerSidebar)
-closeBtn.addEventListener('click', close)
+triggerBtn.addEventListener('click', openSidebar)
+closeBtn.addEventListener('click', closeSidebar)
+like.addEventListener('click', likeProduct)
+dislike.addEventListener('click', dislikeProduct)
 
-function triggerSidebar(){
+function openSidebar(){
     sidebar.style.right = '0%'
 }
-function close(){
+function closeSidebar(){
     sidebar.style.right = '-57%'
+}
+function likeProduct(){
+    like.style.display = 'none'
+    dislike.style.display = 'block'
+}
+
+function dislikeProduct(){
+    like.style.display = 'block'
+    dislike.style.display = 'none'
 }
